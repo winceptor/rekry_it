4addedf23db86200be1a6ddc13521f40fd9885f5
winceptor <creec.dx@gmail.com>
2016-06-22 14:55:46 +0300

    default secret file added

---
+++ config/secret.default.js
@@ -0,0 +1,17 @@
+module.exports={
+	localhostadmin : false,
+	server_port: 80,
+	
+	//DATABASE
+	db_database: 'mongodb://user:pass@server:port/directory',
+	db_secretKey: "",
+	
+	//EMAIL
+	email_host: 'localhost',
+    email_port: 465,
+	email_secure: true, // use SSL
+	email_user: 'user@email',
+    email_pass: 'password',
+	
+	email_sender: 'LUT Collaborative Portal'
+}
