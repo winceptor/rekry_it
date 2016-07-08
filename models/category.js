e632d5552f3c19c0b1d0a067e47047fd891c6fe2
winceptor <creec.dx@gmail.com>
2016-07-08 16:09:05 +0300

    added date for categories

--- models/category.js
+++ models/category.js
@@ -5,6 +5,7 @@ var Schema = mongoose.Schema;
 var CategorySchema = new Schema({
   name: { type: String, default: 'Untitled', unique: false},
   category: { type: String, default: 'other'},
+  date:{ type: Date, default: Date.now },
   jobs: [{type:Schema.Types.ObjectId, ref:'Job'}],
   users: [{type:Schema.Types.ObjectId, ref:'User'}] 
 });
