646f346b5c34d92c354af3028b0af19744758a59
winceptor <creec.dx@gmail.com>
2016-07-12 14:48:05 +0300

    layout fixes

--- public/js/loading.js
+++ public/js/loading.js
@@ -1,16 +1,13 @@
-document.write('<style type="text/css">.loading {opacity: 0.5;} #loadingcontent {display: none;} #loadingimage {display: block; position: fixed; height: 100%; width: 100%; z-index: 9999; background-image: url("/loading.svg"); background-repeat: no-repeat; background-position: 50%; top: 0; left: 0;}</style>');
-$(document).ready(function(){
-	$lc = $("#loadingcontent");
-	$li = $("#loadingimage");
+document.write('<style type="text/css">#loadingcontent {display: none;} #loadingimage {display: block; position: fixed; height: 100%; width: 100%; z-index: 9999; background-image: url("/loading.svg"); background-repeat: no-repeat; background-position: 50%; top: 0; left: 0;}</style>');
 
-	var showloading = function()
-	{
-		$lc.fadeOut();
-		$li.fadeIn('fast');
-	}
+var showloading = function()
+{
+	$("#loadingcontent").fadeOut();
+	$("#loadingimage").fadeIn('fast');
+}
 
-	window.onbeforeunload = showloading;
+window.onbeforeunload = showloading;
 	
-	$lc.fadeIn('slow', function(){
-	})
+$(document).ready(function(){
+	$("#loadingcontent").fadeIn('slow');
 });
