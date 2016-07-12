732253a02c2b3505b01aef9e025262a7de6edb1b
winceptor <creec.dx@gmail.com>
2016-07-12 10:40:41 +0300

    compression 1 (for testing again)

--- server.js
+++ server.js
@@ -62,8 +62,9 @@ mongoose.connect(secret.db_database,function(err){
 
 });
 
+
 //middleware (compression first)
-app.use(compression());
+app.use(compression({level: 1}));
 
 app.use(express.static(__dirname+'/public'));
 app.use(morgan('short'));
