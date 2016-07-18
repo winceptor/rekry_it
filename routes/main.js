888ad12ecc6684db746d1b201e866b858aa93a3f
winceptor <creec.dx@gmail.com>
2016-07-18 16:17:39 +0300

    removed preceding zeros from dates

--- routes/main.js
+++ routes/main.js
@@ -119,8 +119,9 @@ function DateToInput(date) {
 	var dd = date.getDate(); 
 	var mm = date.getMonth()+1; 
 	var yyyy = date.getFullYear(); 
-	if(dd<10){dd="0"+dd} 
-	if(mm<10){mm="0"+mm} 
+	//no need for trailing zeros
+	//if(dd<10){dd="0"+dd} 
+	//if(mm<10){mm="0"+mm} 
 	//return yyyy+"-"+mm+"-"+dd;
 	return dd + "." + mm + "." + yyyy;
 }
