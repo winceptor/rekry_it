fae07b0ba873cdb4cacb44332318b1efc3f1c1f7
winceptor <creec.dx@gmail.com>
2016-07-27 14:10:22 +0300

    all log files can be viewed now

--- routes/logger.js
+++ routes/logger.js
@@ -2,7 +2,9 @@
 
 var FileStreamRotator = require('file-stream-rotator')
 
-var router= require('express').Router();
+var express= require('express');
+
+var router= express.Router();
 
 var fs = require('fs')
 var morgan = require('morgan')
@@ -23,5 +25,46 @@ var accessLogStream = FileStreamRotator.getStream({
 
 // setup the logger
 router.use(morgan('[:date[clf]] :remote-addr :remote-user :method :url :status - :response-time ms', {stream: accessLogStream}))
+
+router.use('/log',function(req,res,next){
+	if (!res.locals.zeroadmins && (!req.user || !req.user.admin)) { return res.redirect("/denied"); }
+	express.static(logDirectory)(req,res,next);
+});
+
+
+router.get('/log',function(req,res,next){
+	if (!res.locals.zeroadmins && (!req.user || !req.user.admin)) { return res.redirect("/denied"); }
+	
+	var files = fs.readdirSync(logDirectory, 'utf8');
+	
+	return res.render('admin/logs',{
+		data: files
+	});
+	
+	/*
+	var filename = res.locals.logfile;
+	
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	try {
+		var data = fs.accessSync('./log/'+filename, fs.F_OK, function(err, data){
+			if (err) {
+				console.log(err);
+				return false; 
+			}
+		});
+		var data = fs.readFileSync('./log/'+filename, 'utf8', function(err, data){
+			if (err) {
+				console.log(err);
+				return false; 
+			}
+		});
+		res.end(data);
+		return true;
+	} catch (e) {
+		//console.log(e);
+		res.end('Could not read ' + filename + ' file!');
+	}
+	return false;*/
+});
 
 module.exports= router;
