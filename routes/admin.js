3356f94a6cebd5bd092a580ddd6624e8e137e2b7
winceptor <creec.dx@gmail.com>
2016-07-27 14:12:22 +0300

    small fix

--- routes/admin.js
+++ routes/admin.js
@@ -11,33 +11,6 @@ router.use(function(req,res,next){
 	if (!res.locals.zeroadmins && (!req.user || !req.user.admin)) { return res.redirect("/denied"); }
 	next();
 });
-
-router.get('/server-log',function(req,res,next){
-	var filename = res.locals.logfile;
-	
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	try {
-		var data = fs.accessSync('./log/'+filename, fs.F_OK, function(err, data){
-			if (err) {
-				console.log(err);
-				return false; 
-			}
-		});
-		var data = fs.readFileSync('./log/'+filename, 'utf8', function(err, data){
-			if (err) {
-				console.log(err);
-				return false; 
-			}
-		});
-		res.end(data);
-		return true;
-	} catch (e) {
-		//console.log(e);
-		res.end('Could not read ' + filename + ' file!');
-	}
-	return false;
-});
-
 	
 var jobRoutes=require('./job-management');
 router.use(jobRoutes);
