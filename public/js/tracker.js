5c2c3c1b8871c5a8860bcc05ea4e2c1c18d86793
winceptor <creec.dx@gmail.com>
2016-08-18 09:54:41 +0300

    tracker autorefresh

--- public/js/tracker.js
+++ public/js/tracker.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
+	
+	var showseconds = 15;
+	var fadeseconds = 3;
+	var entries = $('#trackerlist').children().length;
+	var refreshseconds = showseconds*entries+fadeseconds;
 
 	setInterval(function(){ 
 		var s = $('#trackerlist');
 		var e = $("#trackerlist").children().eq(0);
-		$(e).fadeOut(3000, function(){
+		$(e).fadeOut(1000*fadeseconds, function(){
 			$(s).append($(e));
 			$(e).fadeIn("slow");
 		});
 		
 		
-	},1000*15);//15s interval
+	},1000*showseconds);//15s interval
+	
+	setTimeout(function(){ 
+		location.reload(true);
+	}, 1000*refreshseconds);
 	
 });
